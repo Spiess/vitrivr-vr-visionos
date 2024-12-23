e1c8b7d7a0a3eab9b2905d4100b7d9c7cd9e7e33
Florian Spiess <florian.spiess@unibas.ch>
2024-12-23 11:01:58 +0100

    Initial Commit

---
+++ vitrivr-VR/vitrivr_VRApp.swift
@@ -0,0 +1,17 @@
+//
+//  vitrivr_VRApp.swift
+//  vitrivr-VR
+//
+//  Created by Florian Spiess on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct vitrivr_VRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vitrivr-VRTests/vitrivr_VRTests.swift
@@ -0,0 +1,17 @@
+//
+//  vitrivr_VRTests.swift
+//  vitrivr-VRTests
+//
+//  Created by Florian Spiess on 23.12.2024.
+//
+
+import Testing
+@testable import vitrivr_VR
+
+struct vitrivr_VRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
